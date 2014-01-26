5179b71e182bab78513fca14510ee193b74c61f9
Stefan Billiet <stefanbilliet@MacBook-Pro-van-Stefan.local>
2014-01-26 18:59:37 +0100

    pingpong snippet

---
+++ Chapter1/pingpong.erl
@@ -0,0 +1,15 @@
+-module(pingpong).
+
+-export([run/0]).
+
+run() ->
+	Pid = spawn(fun ping/0),
+	Pid ! self(),
+	receive
+		pong -> ok
+	end.
+
+ping() ->
+	receive
+		From -> From ! pong
+    end.
