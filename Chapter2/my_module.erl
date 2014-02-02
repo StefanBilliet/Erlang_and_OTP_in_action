5bade03952e339b443b9bd4ca4248d5520bb9c11
Stefan Billiet <stefanbilliet@MacBook-Pro-van-Stefan.local>
2014-02-02 22:11:07 +0100

    Chapter 2.5.3 snippet

--- Chapter2/my_module.erl
+++ Chapter2/my_module.erl
@@ -2,10 +2,14 @@
 
 -module(my_module).
 
--export([pie/0, print/1]).
+-export([pie/0, print/1, either_or_both/2]).
 
 pie()->
 	3.14.
 
 print(Term) ->
 	io:format("The value of Term is: ~p.~n", [Term]).
+
+either_or_both(true,B) when is_boolean(B) -> true;
+either_or_both(A,true) when is_boolean(A) -> true;
+either_or_both(false,false) -> false.
