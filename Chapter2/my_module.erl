744ffb87b22bc1bdc9d7387e4b07237bfb14765f
Stefan Billiet <stefanbilliet@MacBook-Pro-van-Stefan.local>
2014-02-03 15:12:19 +0100

    Rest of chapter 2 snippets

--- Chapter2/my_module.erl
+++ Chapter2/my_module.erl
@@ -2,14 +2,26 @@
 
 -module(my_module).
 
+-define(PI, 3.14).
+
 -export([pie/0, 
 		 print/1, 
 		 either_or_both/2,
 		 area/1,
-		 area_case/1]).
+		 area_case/1,
+		 sign/1,
+		 customer_factory/3,
+		 sum/1,
+		 step_two/1,
+		 step_two/2,
+		 revert/1,
+		 tailrevert/1,
+		 tailrevert/2]).
+
+-record(customer, {name="<anonymous>", address, phone}).
 
 pie()->
-	3.14.
+	?PI.
 
 print(Term) ->
 	io:format("The value of Term is: ~p.~n", [Term]).
@@ -34,3 +46,27 @@ area_case(Shape) ->
 		{rectangle, Height, Width} ->
 			Height * Width
 	end.
+
+sign(N) when is_number(N) ->
+	if
+		N > 0 -> positive;
+		N < 0 -> negative;
+		true -> zero
+	end.
+
+customer_factory(Name, Address, Phone) ->
+	#customer{name = Name, address = Address, phone = Phone}.
+
+sum(0) -> 0;
+sum(N) -> sum(N-1) + N.
+
+step_two(N, Total) when N =/= 0 -> step_two(N-1, Total + N);
+step_two(0, Total) -> Total.
+step_two(N) -> step_two(N, 0).
+
+revert([]) -> [];
+revert([X | TheRest]) -> revert(TheRest) ++ [X].
+
+tailrevert(List) -> tailrevert(List, []).
+tailrevert([X | TheRest], Acc) -> tailrevert(TheRest, [X|Acc]);
+tailrevert([], Acc) -> Acc.
